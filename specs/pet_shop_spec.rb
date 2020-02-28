dd7b23962e650ad3bd550a993c57b5163f36bb87
Tamara Bain <tamara@Tamaras-MacBook-Pro.local>
2020-02-28 12:27:06 +0000

    Made test_customer_can_afford_pet__sufficient_fund and __insuficient_funds pass

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -174,17 +174,17 @@ class TestPetShop < Minitest::Test
 
   # --- OPTIONAL ---
 
-  # def test_customer_can_afford_pet__sufficient_funds
-  #   customer = @customers[0]
-  #   can_buy_pet = customer_can_afford_pet(customer, @new_pet)
-  #   assert_equal(true, can_buy_pet)
-  # end
+  def test_customer_can_afford_pet__sufficient_funds
+    customer = @customers[0]
+    can_buy_pet = customer_can_afford_pet(customer, @new_pet)
+    assert_equal(true, can_buy_pet)
+  end
 
-  # def test_customer_can_afford_pet__insufficient_funds
-  #   customer = @customers[1]
-  #   can_buy_pet = customer_can_afford_pet(customer, @new_pet)
-  #   assert_equal(false, can_buy_pet)
-  # end
+  def test_customer_can_afford_pet__insufficient_funds
+    customer = @customers[1]
+    can_buy_pet = customer_can_afford_pet(customer, @new_pet)
+    assert_equal(false, can_buy_pet)
+  end
 
   # def test_customer_can_afford_pet__exact_funds
   #   customer = @customers[2]
