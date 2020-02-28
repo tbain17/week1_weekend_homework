41f6ee4c8f0e0907fa5f448036e16678892c098b
Tamara Bain <tamara@Tamaras-MacBook-Pro.local>
2020-02-28 12:34:32 +0000

    Made test_customer_can_afford_pet__exact_funds pass

--- pet_shop.rb
+++ pet_shop.rb
@@ -75,3 +75,12 @@ def customer_can_afford_pet(customer, new_pet)
   end
   return false
 end
+
+def sell_pet_to_customer(shop, pet, customer)
+  if customer_can_afford_pet(customer, pet) = true
+    remove_customer_cash(customer, pet[:price])
+    add_or_remove_cash(shop, pet[:price])
+    add_pet_to_customer(customer,pet)
+    remove_pet_by_name(shop, pet)
+  end
+end
